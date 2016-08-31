7a1bf190ee82219d530bd2007eebb0a613c312cf
codyjm <cody.moxham@my.jcu.edu.au>
2016-08-31 16:49:02 +1000

    Finish ticket_generator for Prac04

--- Prac04/ticket_generator.py
+++ Prac04/ticket_generator.py
@@ -7,18 +7,24 @@ import random
 MIN_NUMBER = 1
 MAX_NUMBER = 45
 
-def main():
-    ticket_numbers = get_random_numbers()
-    quick_picks = input("How many quick picks? ")
 
-    for line in quick_picks:
+def main():
+
+    quick_picks = int(input("How many quick picks? "))
+    for pick in range(quick_picks):
+        ticket_numbers = get_random_numbers()
+        print(ticket_numbers)
 
 
 def get_random_numbers():
     numbers = []
 
     for number in range(6):
-        numbers.append(random.randint(1, 45))
+        rand_number = random.randint(1, 45)
+        while rand_number in numbers:
+            rand_number = random.randint(1, 45)
+        numbers.append(rand_number)
+
     numbers.sort()
     return numbers
 
