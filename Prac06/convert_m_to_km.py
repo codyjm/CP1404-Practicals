5f0f5f9714a1ed8dca8f2b3b4888543f4f2df0e2
codyjm <cody.moxham@my.jcu.edu.au>
2016-09-13 22:54:28 +1000

    Start conversion exercise TODO complete increment change

---
+++ Prac06/convert_m_to_km.py
@@ -0,0 +1,33 @@
+"""
+Convert miles to kilometres with Kivy GUI
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+
+MILES_TO_KM = 1.60934
+
+
+class ConvertToKm(App):
+    def build(self):
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file("convert_m_to_km.kv")
+        return self.root
+
+    def handle_convert(self):
+        miles = self.get_valid_input()
+        result = miles * MILES_TO_KM
+        self.root.ids.output_label.text = str(result)
+
+    def handle_increment(self, increment):
+        value = self.get_valid_input() + increment
+        self.handle_convert()
+
+    def get_valid_input(self):
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+ConvertToKm().run()
